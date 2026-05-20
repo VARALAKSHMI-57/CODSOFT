16e01f8ca59302ae02382627fa7f865140ae739d
VARALAKSHMI-57 <varalakshmisatti57@gmail.com>
2026-05-20 17:38:29 +0530

    Add files via upload

---
+++ TO-DO LIST APPLICATION.py
@@ -0,0 +1,44 @@
+#TO-DO LIST APPLICATION
+   
+tasks = []
+def show_menu():
+    print("\n===== TO-DO LIST MENU =====")
+    print("1. Add Task")
+    print("2. view Tasks")
+    print("3. Remove Task")
+    print("4. Exit")
+while True:
+    show_menu()
+    choice = input("Enter your choice (1-4):")
+    if choice == "1":
+        task=input("Enter your task:")
+        tasks.append(task)    
+        print("Task added successfully!")
+    elif choice == "2":
+        if len(tasks) == 0:
+            print("No tasks available.")
+        else:
+            print("\nYour Tasks:")
+            for i,task in enumerate(tasks,start=1):
+                print(f"{i}.{task}")
+    elif choice == "3":
+        if len(tasks) == 0:
+            print("No tasks to remove.")
+        else:
+            print("\nYour Tasks:")
+            for i,task in enumerate(tasks,start=1):
+                print(f"{i}. {task}")
+            try:
+                task_num = int(input("Enter task number to remove:"))
+                if 1 <= task_num <= len(tasks):
+                    removed = tasks.pop(task_num - 1)
+                    print(f"Task '{removed}' removed successfully!")
+                else:
+                    print("Invalid task number.")
+            except ValueError:
+                print("Please enter a valid number.")
+    elif choice =="4":
+        print("Exiting To-Do List Application...")
+        break
+    else:
+        print("Invalid choice.please try again.")                                                    
